7d8a1098dcfc9e5b708e3d76920209acedeaa24a
Saurabh Hembade <saurabhhembade9518@gmail.com>
2026-09-13 23:56:16 +0530

    Update CORS for Eventopia production

--- event-backend/index.js
+++ event-backend/index.js
@@ -12,7 +12,7 @@ const fileUpload = require("express-fileupload");
 const aiRecommendationRoutes = require("./routes/aiRecommendationRoutes");
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://eventopia-evm.vercel.app", 
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
